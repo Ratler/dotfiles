fcfae0c3c52f02895f979e19eb5f2ab8f94b1c2a
Stefan Wold <ratler@stderr.eu>
2017-02-08 13:18:47 +0100

    docker shell utility function

---
+++ .bash.d/docker.sh
@@ -0,0 +1,32 @@
+dsh() {
+  local DOCKER_CONTAINERS IFS CONTAINER_ID DOCKER_HEADER
+
+  IFS=$'\n'
+
+  DOCKER_HEADER=$(docker ps | head -1)
+  declare -a DOCKER_CONTAINERS=($(docker ps | tail -n +2 | tac))
+
+  if [ -z $1 ]; then
+    echo "Choose docker container to start shell in:"
+    echo -e "#\t$DOCKER_HEADER"
+    for i in ${!DOCKER_CONTAINERS[@]}; do
+      printf "%s\t%s\n" "$i" "${DOCKER_CONTAINERS[$i]}"
+    done
+    echo -n "Container #> "
+    read ANS
+  else
+    ANS=$1
+  fi
+
+  case "$ANS" in
+    +([0-9]))
+      if [ -n "${DOCKER_CONTAINERS[$ANS]}" ]; then
+        CONTAINER_ID=$(echo ${DOCKER_CONTAINERS[$ANS]} | awk '{ print $1 }')
+        echo "Starting a shell in $CONTAINER_ID"
+        docker exec -ti $CONTAINER_ID bash
+      fi
+    ;;
+    *)
+    ;;
+  esac
+}
