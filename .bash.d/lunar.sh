a2fbab9dc7ea17fcd21c2a37168030873ecc449f
Stefan Wold <ratler@stderr.eu>
2018-04-09 22:19:44 +0200

    new lunar related utility function

--- .bash.d/lunar.sh
+++ .bash.d/lunar.sh
@@ -1,11 +1,22 @@
-llpatch () {
+llpatch() {
   scp $* lunar-linux.org:/var/ftp/pub/lunar/patches/
 }
 
-zlocalapply () {
+zlocalapply() {
   CURMOD=`basename $PWD`
   MBDIR=`dirname $PWD | sed -r 's;(.*moonbase-(core|efl|gnome|kde|other|xfce|xorg)).*;\1;'`
   lvu diff $CURMOD | ( cd $MBDIR ; patch -p1)
 }
+
+unpack() {
+  if [ "$UID" = "0" ]; then
+    lget systemd
+  fi
+  lsh "run_details $1; unpack \$SOURCE"
+}
+
+# module completion for unpack using existing completion provided by Lunar
+complete -F _lunar_comp_modules unpack
+
 
 export -f llpatch zlocalapply
