fafd15c8a6f574b6e15fd5528d11804ef479d6f5
Stefan Wold <ratler@stderr.eu>
2019-10-01 22:43:44 +0200

    Updated a few utility functions

--- .bash.d/lunar.sh
+++ .bash.d/lunar.sh
@@ -10,7 +10,7 @@ zlocalapply() {
 
 unpack() {
   if [ "$UID" = "0" ]; then
-    lget systemd
+    lget $1
   fi
   lsh "run_details $1; unpack \$SOURCE"
 }
@@ -18,5 +18,37 @@ unpack() {
 # module completion for unpack using existing completion provided by Lunar
 complete -F _lunar_comp_modules unpack
 
+mo() {
+  local branch repo
 
-export -f llpatch zlocalapply
+  if [ ! -d "$HOME/src/lunar/moonbase" ]; then
+    mkdir -p "$HOME/src/lunar/moonbase"
+  fi
+
+  pushd "$HOME/src/lunar/moonbase" &> /dev/null
+
+  for repo in core efl gnome kde other xfce xorg gnome3; do
+    if [ ! -d $repo ]; then
+      git clone git@github.com:lunar-linux/moonbase-${repo}.git $repo
+    fi
+
+    pushd $repo &> /dev/null
+    branch=$(git rev-parse --abbrev-ref HEAD)
+    if [[ -n "$1" && "$1" != "$branch" ]]; then
+      git checkout $1
+      branch=$1
+    fi
+
+    if [ "$branch" == "master" ]; then
+      echo "+++ Updating $repo"
+      git pull
+    else
+      echo "+++ Skipping $repo/$branch"
+    fi
+    popd &> /dev/null
+  done
+  cat */aliases > aliases
+  popd &> /dev/null
+}
+
+export -f llpatch zlocalapply mo
